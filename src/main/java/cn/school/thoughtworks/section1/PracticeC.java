c644ac10199e0e2b991d0f34475987be6ccc2104
Via <1627973075@qq.com>
2018-04-09 13:29:55 +0800

    commit section1 PracticeC.java

--- src/main/java/cn/school/thoughtworks/section1/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeC.java
@@ -1,11 +1,13 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class PracticeC {
     List<String> collectSameElements(List<String> collection1, Map<String,List<String>> collection2) {
         //实现练习要求，并改写该行代码。
-        return null;
+        return collection1.stream().filter(n -> collection2.values().stream().flatMap(Collection::stream).collect(Collectors.toList()).contains(n)).collect(Collectors.toList());
     }
 }
