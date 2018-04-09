becb74740aa42d6b8686fa56f1f228c745dd927a
Via <1627973075@qq.com>
2018-04-09 14:37:52 +0800

    commit section2 PractiseC

--- src/main/java/cn/school/thoughtworks/section2/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeC.java
@@ -1,12 +1,24 @@
 package cn.school.thoughtworks.section2;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeC {
     Map<String, Integer> countSameElements(List<String> collection1) {
-        //实现练习要求，并改写该行代码。
-
-        return null;
+        Map<String, Integer> map = new HashMap<>();
+        for(String key : collection1){
+            if (1 == key.length()){
+                int count = map.getOrDefault(key, 0);
+                map.put(key, count+1);
+            }else if (key.charAt(1) == '-' || key.charAt(1) == ':'){
+                int count = map.getOrDefault(key.substring(0,1), 0);
+                map.put(key.substring(0,1), count + Integer.valueOf(key.substring(2)));
+            }else if (key.charAt(1) == '['){
+                int count = map.getOrDefault(key.substring(0,1), 0);
+                map.put(key.substring(0,1), count + Integer.valueOf(key.substring(2, key.length()-1)));
+            }
+        }
+        return map;
     }
 }
