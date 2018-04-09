8f82230fab1f9127e97f4ebdf7933ac01b521661
Via <1627973075@qq.com>
2018-04-09 11:57:40 +0800

    commit session1 PracticeB

--- src/main/java/cn/school/thoughtworks/section1/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeB.java
@@ -1,10 +1,11 @@
 package cn.school.thoughtworks.section1;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class PracticeB {
     List<String> collectSameElements(List<String> collection1, List<List<String>> collection2) {
         //实现练习要求，并改写该行代码。
-        return null;
+        return collection1.stream().filter(n -> collection2.stream().findFirst().get().contains(n)).collect(Collectors.toList());
     }
 }
