9dfeca0d023011dd0ce8f7321a7eeea91ff9cd9c
Via <1627973075@qq.com>
2018-06-01 11:02:58 +0800

    delet invalid package in section2/PracticeB.java

--- src/main/java/cn/school/thoughtworks/section2/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeB.java
@@ -1,12 +1,8 @@
 package cn.school.thoughtworks.section2;
 
-import javafx.beans.binding.MapBinding;
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 public class PracticeB {
     Map<String, Integer> countSameElements(List<String> collection1) {
